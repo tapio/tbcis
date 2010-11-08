6e2a97fc026710f380a0ca681e63da35dbf311b1
tapio <tapiovierros@gmail.com>
2010-11-08 17:33:00 +0200

    Dir walker php function.

--- webui/tbcis.php
+++ webui/tbcis.php
@@ -4,8 +4,28 @@
  * Tiny Bash Continuous Integration System.
  */
 
+
+function tbcis_iterate_results($dir, $func) {
+	$entries = scandir($dir);
+	foreach($entries as $entry) {
+		if($entry == '.' || $entry == '..') {
+			// Skip these
+		} else if(is_dir($entry)) {
+			// Found a result dir
+			$func($entry, $dir.'/'.$entry);
+		} // Ignore non-directories
+	}
+}
+
+
 function tbcis_show() {
 	echo "It works!";
 }
+
+
+function tbcis_print_run_row($dir_path, $id) {
+
+}
+
 
 ?>
