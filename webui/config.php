795eff042889460c8e9ad8af36914f84d16ea315
Tapio Vierros <tapiovierros@gmail.com>
2010-11-08 17:13:45 +0200

    Starting PHP based Web UI.

---
+++ webui/config.php
@@ -0,0 +1,5 @@
+<?php
+$TASKS_ROOT="../tasks"
+$RESULTS_ROOT="../results"
+
+?>
---
+++ webui/tbcis.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * PHP bindings for displaying data from TBCIS,
+ * Tiny Bash Continuous Integration System.
+ */
+
+function tbcis_show() {
+	echo "It works";
+}
+
+?>
