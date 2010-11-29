df4f033ebb8d9e9a0be9e74841f5a7153a1eadc8
Tapio Vierros <tapiovierros@gmail.com>
2010-11-29 20:16:45 +0200

    Caching system for WebUI.

--- webui/cgi/tbcis_webui.sh
+++ webui/cgi/tbcis_webui.sh
@@ -22,29 +22,6 @@ t=`date +%s%N`
 echo Content-type: text/html
 echo ""
 
-# Output some HTML ot start with
-cat << EOF
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta http-equiv="refresh" content="120">
-	<title>Tiny Bash Continuous Integration System WebUI</title>
-	<!--<link href="style.css" rel="stylesheet" type="text/css" />-->
-	<style type="text/css">
-EOF
-# Embed stylesheet for now.
-cat "$TBCIS_WEBUI_ROOT/style.css"
-cat << EOF
-	</style>
-</head>
-<body>
-	<div id="header">
-		<h1>CI Tasks @ $HOSTNAME</h1>
-	</div>
-	<div id="content">
-EOF
-
 create_cell()
 {
 	status=`cat $1.status || echo N/A`
@@ -80,46 +57,99 @@ create_dl_cell()
 	echo "</td>"
 }
 
-# Check that there is tasks
-if [ -d "$TBCIS_RESULTS_ROOT" -a "`ls $TBCIS_RESULTS_ROOT`" ]; then
-	# Iterate tasks
-	cd "$TBCIS_RESULTS_ROOT"
-	for task in *; do
-		echo "<div class=\"task\">";
-		echo "<h3>$task</h3>"
-		( cd $task
-			# Table start
-			echo "<table class=\"runtable\">"
-			echo "<tr><th>Run id</th><th>Config</th><th>Build</th><th>Package</th><th>Test</th><th>Output files</th></tr>"
-			for i in `ls|tac`; do
-				( cd $i
-					echo "<tr>"
-					echo "<td>$i</td>"
-					create_cell config
-					create_cell build
-					create_cell package
-					create_cell test
-					create_dl_cell
-					echo "</tr>"
-				)
-			done
-			echo "</table>"
-		)
-		echo "</div>"
-	done
-else
-	echo "<p>No tasks/runs.</p>"
-fi
-
-tt=`date +%s%N`
-t=$((($tt - $t)/1000000))
-# Add the rest of the HTML
-cat << EOF
+generate_page()
+{
+	# Output some HTML ot start with
+	cat << EOF
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="refresh" content="120">
+	<title>Tiny Bash Continuous Integration System WebUI</title>
+	<!--<link href="style.css" rel="stylesheet" type="text/css" />-->
+	<style type="text/css">
+EOF
+	# Embed stylesheet for now.
+	cat "$TBCIS_WEBUI_ROOT/style.css"
+	cat << EOF
+	</style>
+</head>
+<body>
+	<div id="header">
+		<h1>CI Tasks @ $HOSTNAME</h1>
 	</div>
+
+<div id="content">
+EOF
+
+	# Check that there is tasks
+	if [ -d "$TBCIS_RESULTS_ROOT" -a "`ls $TBCIS_RESULTS_ROOT`" ]; then
+		# Iterate tasks
+		cd "$TBCIS_RESULTS_ROOT"
+		for task in *; do
+			echo "<div class=\"task\">";
+			echo "<h3>$task</h3>"
+			( cd $task
+				# Table start
+				echo "<table class=\"runtable\">"
+				echo "<tr><th>Run id</th><th>Config</th><th>Build</th><th>Package</th><th>Test</th><th>Output files</th></tr>"
+				for i in `ls|tac`; do
+					( cd $i
+						echo "<tr>"
+						echo "<td>$i</td>"
+						create_cell config
+						create_cell build
+						create_cell package
+						create_cell test
+						create_dl_cell
+						echo "</tr>"
+					)
+				done
+				echo "</table>"
+			)
+			echo "</div>"
+		done
+	else
+		echo "<p>No tasks/runs.</p>"
+	fi
+
+	# Add the rest of the HTML
+	cat << EOF
+</div>
+
 	<div id="footer">
-		<small>Powered by TBCIS<span> - Page generated in $t ms</span></small>
+		<small>Powered by TBCIS</small>
 	</div>
 </body>
 </html>
 EOF
+}
+
+# Cache system
+
+TBCIS_WEBUI_CACHE_FILE="/tmp/.tbcis.cache"
+TBCIS_WEBUI_CACHE_TIME=5
+
+if [ ! -e "$TBCIS_WEBUI_CACHE_FILE" ]; then
+	# Create if no cache not found
+	generate_page > "$TBCIS_WEBUI_CACHE_FILE"
+else
+	cache_age=`stat -c %Y "$TBCIS_WEBUI_CACHE_FILE"`
+	cur_time=`date +%s`
+	cache_diff=$(($cur_time-$cache_age))
+	if [ "$cache_diff" -gt "$TBCIS_WEBUI_CACHE_TIME" ]; then
+		# Recreate only if old enough
+		generate_page > "$TBCIS_WEBUI_CACHE_FILE"
+	fi
+fi
+
+# Display cache
+cat "$TBCIS_WEBUI_CACHE_FILE"
+
+# Calculate generation time
+tt=`date +%s%N`
+t=$((($tt - $t)/1000000))
+echo "<!-- Page generated in $t ms -->"
+
 
